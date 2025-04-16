ebc72ac5b9df99a913bbaae0d7554af307586286
Ocean_Dev2 <zakaryabaouali255@gmail.com>
2025-04-16 11:29:33 +0100

    fix return for pharmacy

--- src/services/pharmacies/pharmacy.prescription.service.js
+++ src/services/pharmacies/pharmacy.prescription.service.js
@@ -6,12 +6,69 @@ export async function getPrescriptionsByPharmacy(pharmacyId) {
   if (!pharmacyId) {
     throw new Error('Pharmacy ID is required');
   }
-
+  
   const prescriptions = await prisma.prescription.findMany({
     where: {
       pharmacyId: pharmacyId,
     },
+    include: {
+      patient: {
+        include: {
+          user: {
+            select: {
+              firstname: true,
+              lastname: true,
+              email: true,
+            },
+          },
+        },
+      },
+      pharmacy: {
+        include: {
+          user: {
+            select: {
+              firstname: true,
+              lastname: true,
+              email: true,
+            },
+          },
+        },
+      },
+      doctor: {
+        include: {
+          user: {
+            select: {
+              firstname: true,
+              lastname: true,
+              email: true,
+            },
+          },
+        },
+      },
+    }
   });
-
-  return prescriptions;
+  
+  // Map over prescriptions to add concatenated name fields
+  const enhancedPrescriptions = prescriptions.map(prescription => {
+    return {
+      id: prescription.id,
+      patient: {
+        ...prescription.patient,
+        fullName: `${prescription.patient.user.firstname} ${prescription.patient.user.lastname}`,
+        user: prescription.patient.user
+      },
+      pharmacy: {
+        ...prescription.pharmacy,
+        fullName: `${prescription.pharmacy.user.firstname} ${prescription.pharmacy.user.lastname}`,
+        user: prescription.pharmacy.user
+      },
+      doctor: {
+        ...prescription.doctor,
+        fullName: `${prescription.doctor.user.firstname} ${prescription.doctor.user.lastname}`,
+        user: prescription.doctor.user
+      }
+    };
+  });
+  
+  return enhancedPrescriptions;
 }
